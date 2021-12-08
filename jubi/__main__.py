6966ca207b8e64702ee07e18846b1070da6370be
JeffTheK <jeff144@protonmail.com>
2021-12-09 00:00:25 +0200

    Add basic calendar printig

--- jubi/__main__.py
+++ jubi/__main__.py
@@ -1,2 +1,18 @@
+import calendar
+from calendar import monthrange
+from datetime import datetime
+
 def main():
-    print("hello world")
+    current_time = datetime.today()
+    days_count = monthrange(current_time.year, current_time.month)[1]
+    print("Su Mo Tu We Th Fr Sa")
+    days = calendar.monthcalendar(current_time.year, current_time.month)
+    for row in days:
+        for char in row:
+            if char != 0 and char < 10:
+                print(" " + str(char) + " ", end="")
+            elif char != 0 and char >= 10:
+                print(str(char) + " ", end="")
+            else:
+                print("   ", end="")
+        print()
